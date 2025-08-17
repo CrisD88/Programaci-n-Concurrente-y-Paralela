5f0c7e94fb4988ad3bcc07d8fac6fdc2cc18321e
Cristopher Damian Sanchez Santiago <cristopher.sanchezs@alumno.buap.mx>
2025-08-17 01:31:45 -0600

    Subida de codigo Figuras

---
+++ Hilos_Fig_Geo/Figuras_Geo.java
@@ -0,0 +1,9 @@
+
+import java.util.Scanner;
+
+public class Figuras_Geo {
+    public static void main(String[] args) {
+        System.out.println("\nBienvenido al programa de figuras geométricas\n");
+    }
+    
+}
